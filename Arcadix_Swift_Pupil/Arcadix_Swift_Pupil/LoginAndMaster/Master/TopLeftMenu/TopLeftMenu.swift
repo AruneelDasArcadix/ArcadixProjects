8333283c74b253ad57c4cc81147dbcbadfb2045e
mrigarecad <mriganka.goswami@arcadix.com>
2023-06-12 11:54:08 +0530

    Create TopLeftMenu.swift

---
+++ Arcadix_Swift_Pupil/Arcadix_Swift_Pupil/LoginAndMaster/Master/TopLeftMenu/TopLeftMenu.swift
@@ -0,0 +1,20 @@
+//
+//  TopLeftMenu.swift
+//  Arcadix_Swift_Pupil
+//
+//  Created by admin on 09/06/23.
+//
+
+import SwiftUI
+
+struct TopLeftMenu: View {
+    var body: some View {
+        Text("TopLeftMenu")
+    }
+}
+
+struct TopLeftMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        TopLeftMenu()
+    }
+}
